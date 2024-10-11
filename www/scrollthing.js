5ee97994e94d4ac191eb3960d6f634f6ed1296dc
kaneryu <kaneryu.yt@gmail.com>
2024-10-11 13:47:42 -0500

    new v2 in 1 commit

--- www/scrollthing.js
+++ www/scrollthing.js
@@ -1,29 +1,9 @@
-addEventListener("wheel", (event) => {});
+addEventListener('click', updateDiv);
 
-onwheel = (event) => {
-    var doc = document.body;
-    var pageSize = doc.scrollHeight - doc.clientHeight;
-    var start = window.scrollY;
-    var end = event.deltaY < 0 ? 0 : pageSize;
-    var duration = 1000; // duration of the animation in milliseconds
-    var startTime = null;
-    console.log("scrolling");
-    
-    function animateScroll(timestamp) {
-    if (!startTime) startTime = timestamp;
-        var elapsed = timestamp - startTime;
-        var progress = Math.min(elapsed / duration, 1); // progress ranges from 0 to 1
-        var ease = easeOutCubic(progress);
-        window.scrollTo(0, start + (end - start) * ease);
+function updateDiv() {
+    var div = document.getElementById('contact');
+    div.classList.toggle('visible');
 
-        if (progress < 1) {
-            requestAnimationFrame(animateScroll);
-        }
-    }
-
-    requestAnimationFrame(animateScroll);
-};
-
-function easeOutCubic(x) {
-  return 1 - Math.pow(1 - x, 3);
+    var clickhint = document.getElementById('clickhint');
+    clickhint.classList.toggle('visible');
 }
